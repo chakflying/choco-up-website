dbff93d5eb8a4236d703679164e4c6b22bdf541f
Nelson Chan <chakflying@hotmail.com>
2023-12-19 21:07:39 +0800

    Content: Update page meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import type React from "react";
+import type { Metadata } from "next";
 import Navbar from "@/components/navbar/navbar";
 import Hero from "@/components/hero";
 import Growth from "@/components/growth";
 import SuccessStory from "@/components/success-story";
 import Testimonials from "@/components/testimonials";
 import Footer from "@/components/footer";
+
+export const metadata: Metadata = {
+  title: "Choco Up: Growth capital for fast-growing businesses",
+  description: "Asia's #1 revenue-based financing and growth platform. Get up to USD 5M with no equity dilution. Apply in minutes and get funded within 48 hours.",
+};
 
 /**
  * Home page
